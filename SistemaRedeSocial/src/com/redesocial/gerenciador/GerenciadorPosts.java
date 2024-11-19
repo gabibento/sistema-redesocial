7e2369c5161a06a728f2b5bca28a736c96ac7664
GABRIELLA MAUREA BENTO <gabriella_bento@estudante.sesisenai.org.br>
2024-11-18 21:36:01 -0300

    feat: adicição de atributos, constutor e método validarPost em GerenciadorPosts

--- SistemaRedeSocial/src/com/redesocial/gerenciador/GerenciadorPosts.java
+++ SistemaRedeSocial/src/com/redesocial/gerenciador/GerenciadorPosts.java
@@ -1,2 +1,25 @@
-package com.redesocial.gerenciador;public class GerenciadorPosts {
+package com.redesocial.gerenciador;
+
+import com.redesocial.exception.ValidacaoException;
+import com.redesocial.modelo.Post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorPosts {
+    private List<Post> posts;
+    private int proximoId;
+
+    public GerenciadorPosts(){
+        posts = new ArrayList<>();
+        proximoId = 1;
+    }
+
+    private void validarPost(Post post){
+        if(post.getConteudo().isEmpty() || post.getConteudo() == null){
+            throw new ValidacaoException("Conteúdo do post não pode ser vazio");
+        }
+    }
+
+
 }
