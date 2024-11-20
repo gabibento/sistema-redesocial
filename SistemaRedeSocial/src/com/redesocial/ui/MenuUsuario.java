856fabfc46271ff517e83a9e4bfa85c19e3e566a
Gabriella <gabriella.mabento@gmail.com>
2024-11-20 15:23:14 -0300

    feat: implementação método CriarPost na classe MenuUsuario

--- SistemaRedeSocial/src/com/redesocial/ui/MenuUsuario.java
+++ SistemaRedeSocial/src/com/redesocial/ui/MenuUsuario.java
@@ -1,13 +1,32 @@
 package com.redesocial.ui;
 
+import com.redesocial.gerenciador.GerenciadorPosts;
+import com.redesocial.modelo.Post;
 import com.redesocial.modelo.Usuario;
+import com.redesocial.utils.LerEntrada;
 
 public class MenuUsuario {
-    Usuario usuario;
+    private final Usuario usuario;
+    private final GerenciadorPosts gerenciadorPosts;
+
     public MenuUsuario(Usuario usuario){
         this.usuario = usuario;
+        gerenciadorPosts = new GerenciadorPosts();
     }
     public void exibirMenu(){
 
     }
+    private void criarPost(){
+       try{
+           System.out.println("=== Novo Post ===");
+
+           String conteudo = LerEntrada.lerEntradaString("Digite seu post:\n");
+
+           gerenciadorPosts.criar(new Post(usuario, conteudo));
+           System.out.println("Post publicado com sucesso");
+       }catch (Exception e){
+           System.out.println("Erro ao criar post: " + e.getMessage());
+       }
+    }
+
 }
